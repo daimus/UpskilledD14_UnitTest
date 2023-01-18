2b917201874e67eacbd9e2dde31a8f14eed2bfd3
Muhammad Daimus Suudi <daimus.suudi@gmail.com>
2023-01-18 21:26:02 +0700

    chore: remove unused import

--- src/main/java/id/daimus/cleanarchitecture/CleanArchitectureApplication.java
+++ src/main/java/id/daimus/cleanarchitecture/CleanArchitectureApplication.java
@@ -2,10 +2,6 @@ package id.daimus.cleanarchitecture;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 @SpringBootApplication
 public class CleanArchitectureApplication {
     public static void main(String[] args) {
