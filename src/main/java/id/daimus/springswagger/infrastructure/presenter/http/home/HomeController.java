4f6de46b6d48e11a960fc27149777b0ae2272b3d
Muhammad Daimus Suudi <daimus.suudi@gmail.com>
2023-01-20 16:37:51 +0700

    chore: change welcome text

--- src/main/java/id/daimus/springswagger/infrastructure/presenter/http/home/HomeController.java
+++ src/main/java/id/daimus/springswagger/infrastructure/presenter/http/home/HomeController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping
     public ResponseEntity<String> home(){
-        return ResponseEntity.ok("Authenticated");
+        return ResponseEntity.ok("Hello World");
     }
 }
