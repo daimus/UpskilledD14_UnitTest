9a310144248edb67c4fa05b595925f75938c8925
Muhammad Daimus Suudi <daimus.suudi@gmail.com>
2023-01-21 19:15:53 +0700

    chore: remove validation, let product service handle it

--- src/main/java/id/daimus/springswagger/infrastructure/data/jpa/product/ProductEntity.java
+++ src/main/java/id/daimus/springswagger/infrastructure/data/jpa/product/ProductEntity.java
@@ -20,15 +20,11 @@ public class ProductEntity {
     @Column(name="id")
     private Long id;
     @Column(name="name", unique = true)
-    @NotEmpty(message = "The full name is required.")
     private String name;
     @Column(name="description")
-    @NotEmpty(message = "The description is required.")
     private String description;
     @Column(name="stock")
-    @Min(value = 1, message = "The minimum stock is 1")
     private int stock;
     @Column(name="price")
-    @Min(value = 1, message = "The minimum price is 1")
     private int price;
 }
