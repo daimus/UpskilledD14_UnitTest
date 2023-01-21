03d6063c51c5bdccc31922fbc13755e5da944640
Muhammad Daimus Suudi <daimus.suudi@gmail.com>
2023-01-21 19:18:07 +0700

    refactor: change delete product return type to bool

--- src/main/java/id/daimus/springswagger/application/product/service/DeleteProductService.java
+++ src/main/java/id/daimus/springswagger/application/product/service/DeleteProductService.java
@@ -14,11 +14,12 @@ import java.util.Optional;
 public class DeleteProductService implements DeleteProductUseCase {
     private final ProductRepository productRepository;
     @Override
-    public void deleteProduct(Long id) throws DataNotFoundException {
+    public boolean deleteProduct(Long id) throws DataNotFoundException {
         Optional<Product> product = productRepository.getProductById(id);
         if (product.isEmpty()){
             throw new DataNotFoundException(String.format("No product with id %s", id));
         }
         productRepository.deleteProduct(id);
+        return true;
     }
 }
--- src/main/java/id/daimus/springswagger/application/product/usecase/DeleteProductUseCase.java
+++ src/main/java/id/daimus/springswagger/application/product/usecase/DeleteProductUseCase.java
@@ -3,5 +3,5 @@ package id.daimus.springswagger.application.product.usecase;
 import id.daimus.springswagger.shared.exception.DataNotFoundException;
 
 public interface DeleteProductUseCase {
-    void deleteProduct(Long id) throws DataNotFoundException;
+    boolean deleteProduct(Long id) throws DataNotFoundException;
 }
