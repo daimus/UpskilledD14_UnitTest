7a9ec74c717685ac10a0e41648bd81f27626465b
Muhammad Daimus Suudi <daimus.suudi@gmail.com>
2023-01-21 19:17:11 +0700

    feat: add product validation to service layer

--- src/main/java/id/daimus/springswagger/application/product/service/CreateProductService.java
+++ src/main/java/id/daimus/springswagger/application/product/service/CreateProductService.java
@@ -3,15 +3,26 @@ package id.daimus.springswagger.application.product.service;
 import id.daimus.springswagger.application.product.entity.Product;
 import id.daimus.springswagger.application.product.repository.ProductRepository;
 import id.daimus.springswagger.application.product.usecase.CreateProductUseCase;
+import jakarta.validation.*;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CreateProductService implements CreateProductUseCase {
     private final ProductRepository productRepository;
+    ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    Validator validator = factory.getValidator();
     @Override
     public Product createProduct(Product product) {
+        Set<ConstraintViolation<Product>> violations = validator.validate(product);
+        if (!violations.isEmpty()){
+            throw new ConstraintViolationException(violations);
+        }
         return productRepository.createProduct(product);
     }
 }
